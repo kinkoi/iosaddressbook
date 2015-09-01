0d32b3933484e3b4e251b4b179d055a7e73eaf56
klo <kinkoi.lo@symphony.com>
2015-09-01 11:48:20 -0700

    Remove unused files. Add more contacts to the contact file.

--- populate-contacts/Contact.swift
+++ populate-contacts/Contact.swift
@@ -3,23 +3,49 @@
 //  populate-contacts
 //
 //  Created by Kinkoi Lo  on 8/31/15.
-//  Copyright (c) 2015 Kinkoi Lo . All rights reserved.
+//
+//  Copyright (c) 2015 Kinkoi Lo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
 //
 
 import Foundation
 import UIKit
 
 class Contact: Printable{
-    var firstName = "null"
-    var lastName = "null"
-    var email = "null"
-    var photo = "null"
+    var firstName = ""
+    var lastName = ""
+    var email = ""
+    var photo: String? = nil
     
     var description : String {
-        return firstName + " " + lastName + " " + email + " " + photo
+        if let photoName = self.photo {
+            return firstName + " " + lastName + " " + email + " " + photoName;
+        }
+        return firstName + " " + lastName + " " + email;
     }
     
-    func photoImage() -> NSData! {
-        return  UIImagePNGRepresentation(UIImage(named: self.photo))
+    func photoImage() -> NSData? {
+        if let photoFile = self.photo {
+            return  UIImagePNGRepresentation(UIImage(named: photoFile))
+        }
+        return nil;
     }
 }
--- populate-contacts/ViewController.swift
+++ populate-contacts/ViewController.swift
@@ -3,15 +3,45 @@
 //  populate-contacts
 //
 //  Created by Kinkoi Lo  on 8/31/15.
-//  Copyright (c) 2015 Kinkoi Lo . All rights reserved.
+//
+//  Copyright (c) 2015 Kinkoi Lo 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
 import UIKit
 import SwiftCSV
 import AddressBook
 
-class ViewController: UIViewController {
+struct Constants {
+    struct HEADER {
+        static let email = "email"
+        static let firstName = "first name"
+        static let lastName = "last name"
+        static let imageName = "image"
+    }
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
     let addressBookRef: ABAddressBook = ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
 
     override func viewDidLoad() {
@@ -21,6 +51,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func populateContacts(sender: UIButton) {
+        textView.text! = "";
         csvImportHelper()
     }
     
@@ -28,27 +59,37 @@ class ViewController: UIViewController {
         let path = NSBundle.mainBundle().pathForResource("contacts", ofType: "csv")
         var error:NSErrorPointer = nil
         if let url = NSURL.fileURLWithPath(path!) {
-            
             if let csv = CSV(contentsOfURL: url, error: error) {
                 let rows = csv.rows
                 for row in rows {
                     let contact = Contact()
-                    contact.email = row["email"]!
-                    contact.firstName = row["first name"]!
-                    contact.lastName = row["last name"]!
-                    contact.photo = row["image"]!
+                    if let email = row[Constants.HEADER.email]?.stringByReplacingOccurrencesOfString(" ", withString: "") {
+                        contact.email = email
+                    }
+                    if let firstName = row[Constants.HEADER.firstName]?.stringByReplacingOccurrencesOfString(" ", withString: "") {
+                        contact.firstName = firstName
+                    }
+                    if let lastName = row[Constants.HEADER.lastName]?.stringByReplacingOccurrencesOfString(" ", withString: "") {
+                        contact.lastName = lastName
+                    }
+                    if let imageName = row[Constants.HEADER.imageName]?.stringByReplacingOccurrencesOfString(" ", withString: "") {
+                        contact.photo = imageName
+                    }
                     addContact2AddressBook(contact)
                 }
                 if ABAddressBookHasUnsavedChanges(addressBookRef) {
                     var err: Unmanaged<CFErrorRef>? = nil
                     // Use "&err" instead of "err", otherwise it will crash.
                     let result = ABAddressBookSave(addressBookRef, &err)
+                    var message = ""
                     if result {
-                        println("Data saved to address book")
+                        message = "Data saved to address book"
                     } else {
-                        println("Failed to update address book")
+                        message = "Failed to update address book"
                     }
-                    
+                    var alert = UIAlertController(title: "", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                    presentViewController(alert, animated: true, completion: nil)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
                 }
             }
         }
@@ -60,14 +101,16 @@ class ViewController: UIViewController {
                 if granted {
                     println("Granted access to address book")
                 } else {
-                    println("Address book access declined")
+                    var alert = UIAlertController(title: "", message: "Need address book access to update record", preferredStyle: UIAlertControllerStyle.Alert)
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
                 }
             }
         }
     }
     
     func addContact2AddressBook(contact: Contact) {
-        println(contact)
+        textView.text! += contact.description + "\n";
         let addressRecord: ABRecordRef = ABPersonCreate().takeRetainedValue()
         ABRecordSetValue(addressRecord, kABPersonFirstNameProperty, contact.firstName, nil)
         ABRecordSetValue(addressRecord, kABPersonLastNameProperty, contact.lastName, nil)
@@ -76,7 +119,9 @@ class ViewController: UIViewController {
             ABPropertyType(kABStringPropertyType)).takeRetainedValue()
         ABMultiValueAddValueAndLabel(emailAddress, contact.email, kABHomeLabel, nil)
         ABRecordSetValue(addressRecord, kABPersonEmailProperty, emailAddress, nil)
-        ABPersonSetImageData(addressRecord, contact.photoImage(), nil)
+        if let contactImage = contact.photoImage() {
+            ABPersonSetImageData(addressRecord, contactImage, nil)
+        }
         ABAddressBookAddRecord(addressBookRef, addressRecord, nil);
     }
 
